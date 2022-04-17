1757db33bf6f29d7264bf5660b944261b0ada190
takuya1602 <99979189+takuya1602@users.noreply.github.com>
2022-04-17 17:32:09 +0900

    second commit

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         "NAME": os.environ["DB_NAME"],
         "USER": os.environ["DB_USER"],
         "PASSWORD": os.environ["DB_PASSWORD"],
-        "HOST": os.environ["DB_POST"],
+        "HOST": os.environ["DB_HOST"],
         "PORT": os.environ["DB_PORT"],
     }
 }
